cf8969e2b441adcb6e6cf46dfa2147c1f37b6b43
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-10-14 21:39:00 -0300

    terminando de configurar a rota post,associando uma chave estrangeira a um id  e corrigindo a migration de address

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 
 const express = require('express');
 const UserController = require('./controller/userController')
+const AdressController = require('./controller/adressController')
 
 const routes = express.Router();
 
     
-routes.get('/', (req, res) => {
-    res.json({conectado:'conectado meu truta'})
-})
-
+routes.get('/users',UserController.show);
 routes.post('/users', UserController.store )
     
+    
+routes.post('/users/:user_id/addresses', AdressController.store)
 
 module.exports = routes;
